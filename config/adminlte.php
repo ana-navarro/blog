84f59ad8b9326d08fc7fe020edec67a5204a1d1c
ana-navarro <62345070+ana-navarro@users.noreply.github.com>
2020-09-16 15:24:26 -0300

    Fixing the sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'PlayerXP',
+    'title' => 'AdminLTE 3',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Player</b>XP',
+    'logo' => '<b>Admin</b>LTE',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'PlayerXP',
+    'logo_img_alt' => 'AdminLTE',
 
     /*
     |--------------------------------------------------------------------------
@@ -101,12 +101,12 @@ return [
     |
     */
 
-    'classes_auth_card' => 'bg-gradient-dark',
+    'classes_auth_card' => 'card-outline card-primary',
     'classes_auth_header' => '',
-    'classes_auth_body' => 'bg-gradient-dark',
-    'classes_auth_footer' => 'text-center',
-    'classes_auth_icon' => 'text-light',
-    'classes_auth_btn' => 'btn-flat btn-light',
+    'classes_auth_body' => '',
+    'classes_auth_footer' => '',
+    'classes_auth_icon' => '',
+    'classes_auth_btn' => 'btn-flat btn-primary',
 
     /*
     |--------------------------------------------------------------------------
@@ -192,7 +192,7 @@ return [
 
     'logout_url' => 'logout',
 
-    'login_url' => 'login',
+    'login_url' => 'login17',
 
     'register_url' => 'register',
 
--- resources/views/vendor/adminlte/partials/sidebar/menu-item.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/menu-item.blade.php
@@ -1,25 +1,66 @@
 @inject('menuItemHelper', \JeroenNoten\LaravelAdminLte\Helpers\MenuItemHelper)
 
-@if ($menuItemHelper->isHeader($item))
 
-    {{-- Header --}}
-    <li @if(isset($item['id'])) id="{{ $item['id'] }}" @endif class="nav-header">
-        {{ is_string($item) ? $item : $item['header'] }}
-    </li>
-
-@elseif ($menuItemHelper->isSearchBar($item))
-
-    {{-- Search form --}}
-    @include('adminlte::partials.sidebar.menu-item-search-form')
-
-@elseif ($menuItemHelper->isSubmenu($item))
-
-    {{-- Treeview menu --}}
-    @include('adminlte::partials.sidebar.menu-item-treeview-menu')
-
-@elseif ($menuItemHelper->isLink($item))
-
-    {{-- Link --}}
-    @include('adminlte::partials.sidebar.menu-item-link')
-
+@if ($menuItemHelper->isSubmenu($item))
+@if (Auth::check())
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="info text-white">
+            <p>Bem-vindo <span class="text-uppercase">{{ Auth::user()->name }}</span> !</p>
+            
+        </div>
+      </div>
+          
+@endif
+<div class="sidebar">
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+        <li class="nav-item has-treeview menu-open">
+          <a href="#" class="nav-link">
+            <p>
+              Posts
+              <i class="fas fa-angle-left float right"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{ route('posts.index') }}" class="nav-link">
+                <p>Biblioteca de Posts</p>
+              </a>
+              <li class="nav-item">
+              <a href="{{ route('posts.create') }}" class="nav-link">
+                <p>Adicionar um novo Post</p>
+              </a>
+            </li>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+        <li class="nav-item has-treeview menu-open">
+          <a href="#" class="nav-link">
+            <p>
+              Usuários
+              <i class="fas fa-angle-left float right"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{ route('posts.index') }}" class="nav-link">
+                <p>Biblioteca de Posts</p>
+              </a>
+              <li class="nav-item">
+              <a href="{{ route('posts.create') }}" class="nav-link">
+                <p>Adicionar um novo Post</p>
+              </a>
+            </li>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  
 @endif
