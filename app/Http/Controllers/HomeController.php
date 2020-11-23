a45554b02e1919caddd1ddc50c9b9b3aa78bc5fe
Ana Navarro <ana.elisacn.98@gmail.com>
2020-11-23 16:00:07 -0300

    Backup

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Post;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -25,9 +26,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $mostcomment = Cache::remember('mostCommented', now()->addSeconds(10), function(){
+            return Post::mostCommented()->take(5)->get();
+        });
+        $mostactive = Cache::remember('mostActive', now()->addSeconds(10), function(){
+            return User::withMostPosts()->take(5)->get();
+        });
         return view('home', [
-            'mostCommented' => Post::mostCommented()->take(5)->get(),
-            'mostActive' => User::withMostPosts()->take(5)->get(),
+            'mostCommented' => $mostcomment,
+            'mostActive' => $mostactive,
         ]);
     }
     public function showChangePasswordForm(){
